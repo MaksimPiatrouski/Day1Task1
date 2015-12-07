0cc33ef37c940beca6641fa186bd9955a8657e07
Maksim Piatrouski <Maksim_Piatrouski@epam.com>
2015-12-07 13:51:55 +0300

    first commit

---
+++ Main/Runner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Utils;
+
+namespace Main
+{
+    public class Runner
+    {
+        public static void Main()
+        {
+            double number;
+            double x = 0;
+            double n = 0;
+            double p = 0;
+
+            Console.Write("Enter number: ");
+            String aString = Console.ReadLine();
+            if (double.TryParse(aString, out number))
+                x = number;
+
+            Console.Write("Enter root: ");
+            String nString = Console.ReadLine();
+            if (double.TryParse(nString, out number))
+                n = number;
+
+            Console.Write("Enter precision: ");
+            String pString = Console.ReadLine();
+            if (double.TryParse(pString, out number))
+                p = number;
+
+            double result = MathRoot.GetRoot(x, n, p);
+
+            Console.WriteLine("/nResult: " + result);
+
+            //compare result with Math.Pow result
+            double resultPow = Math.Pow(x, 1/n);
+
+            result = Math.Round(result, Convert.ToInt32(n));
+            resultPow = Math.Round(resultPow, Convert.ToInt32(n));
+
+            bool equalResults;
+            if (result == resultPow)
+            {
+                equalResults = true;
+            }
+            else
+            {
+                equalResults = false;
+            }
+            Console.WriteLine(equalResults);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Utils/MathRoot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class MathRoot
+    {
+        public static double GetRoot(double number, double n, double p)
+        {
+            double[] x = new double[2];
+            x[0] = number;
+            x[1] = number / n;
+            while (Math.Abs(x[0] - x[1]) > p)
+            {
+                x[1] = x[0];
+                x[0] = (1 / n) * (((n - 1) * x[1]) + (number / Math.Pow(x[1], n - 1)));
+            }
+           return x[0];
+        }
+    }
+}
